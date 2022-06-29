5ca18a3a8bc5baab004890444e520587778b3bee
alisson <alisson.arruda@techsavvy.com.br>
2022-06-29 15:07:33 -0300

    request order

--- src/components/Table-Medicine/modal-create-medicine.js
+++ src/components/Table-Medicine/modal-create-medicine.js
@@ -5,10 +5,16 @@ import "../../styles/table-modal-styles.css";
 const ModalCreateMedicine = () => {
   const [data, setData] = useState([]);
   const [, setUpdateData] = useState(true);
+
   const [showModal, setShowModal] = useState(false);
+  const [showModalMedicine, setShowModalMedicine] = useState(false);
 
   const openCloseModal = () => {
     setShowModal(!showModal);
+  };
+
+  const openCloseModalMedicine = () => {
+    setShowModalMedicine(!showModalMedicine);
   };
 
   const [medicineSelected, setMedicineSelected] = useState({
@@ -32,7 +38,7 @@ const ModalCreateMedicine = () => {
     console.log(medicineSelected);
   };
 
-  const createMaterial = async () => {
+  const createMedicine = async () => {
     delete medicineSelected.uuid;
     await api
       .post("medicines", medicineSelected)
@@ -40,6 +46,20 @@ const ModalCreateMedicine = () => {
         setData(data.concat(res.data));
         setUpdateData(true);
         openCloseModal();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const createOrder = async () => {
+    delete medicineSelected.uuid;
+    await api
+      .post("order", medicineSelected)
+      .then((res) => {
+        setData(data.concat(res.data));
+        setUpdateData(true);
+        openCloseModalMedicine();
       })
       .catch((error) => {
         console.log(error);
@@ -60,7 +80,15 @@ const ModalCreateMedicine = () => {
           >
             Novo Medicamento
           </button>
+          <button
+            className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent 
+                      rounded-md shadow-sm text-base font-normal text-white bg-[#2D8AE0] active:bg-[#2D8AE0] hover:bg-[#2E66FF]"
+            onClick={() => openCloseModalMedicine()}
+          >
+            Solicitar saída de Medicamento
+          </button>
         </nav>
+
         {/*MODAL DE CADASTRO*/}
         {showModal ? (
           <>
@@ -152,7 +180,82 @@ const ModalCreateMedicine = () => {
                       className="bg-[#2D8AE0] active:bg-[#2D8AE0] hover:bg-[#2E66FF] text-white font-bold uppercase text-sm px-6 py-3 rounded shadow 
                       outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                       type="button"
-                      onClick={() => createMaterial()}
+                      onClick={() => createMedicine()}
+                    >
+                      Enviar
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          </>
+        ) : null}
+
+        {/*MODAL DE SOLICITAÇÃO DE SÁIDA*/}
+        {showModalMedicine ? (
+          <>
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+              <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                {/*content*/}
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                  {/*header*/}
+                  <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                    <h3 className="text-3xl font-semibold text-gray-[#2D8AE0]">
+                      Solicitar saída de medicamento
+                    </h3>
+                    <button
+                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                      onClick={() => setShowModalMedicine(false)}
+                    >
+                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                        ×
+                      </span>
+                    </button>
+                  </div>
+                  {/*body*/}
+                  <div className="relative p-6 flex-auto">
+                    <label className="text-gray-500">
+                      Nome do medicamento:
+                    </label>
+                    <input
+                      type="text"
+                      className="border-color"
+                      name="name"
+                      onChange={handleChange}
+                    />
+                    <label className="text-gray-500">Quantidade:</label>
+                    <input
+                      type="text"
+                      className="border-color"
+                      name="qnty"
+                      onChange={handleChange}
+                    />{" "}
+                    <br />
+                    <label className="text-gray-500">
+                      Motivo da solicitação:
+                    </label>
+                    <input
+                      type="text"
+                      className="border-color"
+                      name="motive"
+                      onChange={handleChange}
+                    />
+                  </div>
+                  {/*footer*/}
+                  <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                    <button
+                      className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => setShowModalMedicine(false)}
+                    >
+                      Fechar
+                    </button>
+                    <button
+                      className="bg-[#2D8AE0] active:bg-[#2D8AE0] hover:bg-[#2E66FF] text-white font-bold uppercase text-sm px-6 py-3 rounded shadow 
+                      outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => createOrder()}
                     >
                       Enviar
                     </button>
