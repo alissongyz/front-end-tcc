73cf2d4f5e7047684f587fba8c724b131c24df4f
Daniel Junior <junior.dalcoli@hotmail.com>
2022-07-05 22:18:21 -0300

    consumindo fluxo de login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,55 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { IconContext } from "react-icons/lib";
 import { FaLastfmSquare } from "react-icons/fa";
+import api from "../../utils/api";
+import React, { useState } from "react";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [cookies, setCookie] = useCookies(["auth"]);
+  const navigate = useNavigate();
+
+  let [passError, setPassError] = useState();
+  let [userError, setUserError] = useState();
+
+  const [userSelected, setUserSelected] = useState({
+    username: "",
+    password: ""
+  });
+
+
+  const autenticateUser = async () => {
+    await api
+      .post("auth/login", userSelected)
+      .then((res) => {
+          setCookie("auth", res.data.token, {
+            path: "/"
+          });
+          setPassError();
+          setUserError();
+          navigate('/home');
+      })
+      .catch((error) => {
+        if(error.response.status === 400){
+          setPassError(true);
+        } else if(error.response.status === 401) {
+          setUserError(true);
+        }
+      });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setUserSelected({
+      ...userSelected,
+      [name]: value,
+    });
+    console.log(userSelected);
+  };
+
+
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -11,7 +58,7 @@ export default function Login() {
             <FaLastfmSquare className= "mx-auto h-12 w-auto" color="#2D8AE0" /> 
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Entre com sua conta</h2>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <div className="mt-8 space-y-6">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -20,15 +67,16 @@ export default function Login() {
                 </label>
                 <input
                   id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  name="username"
+                  type="username"
+                  autoComplete="username"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className={`appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm ${userError ? "userError" : ""}`}
                   placeholder="Username"
+                  onChange={handleChange}
                 />
               </div>
-              <div>
+              <div >
                 <label htmlFor="password" className="sr-only">
                   Password
                 </label>
@@ -38,8 +86,9 @@ export default function Login() {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className={`appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm ${passError ? "passError" : ""}`}
                   placeholder="Password"
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -48,6 +97,7 @@ export default function Login() {
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#2D8AE0] active:bg-[#2D8AE0] hover:bg-[#2E66FF] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={() => autenticateUser()}
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <IconContext.Provider value={{ color: "#5e5e60" }} className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
@@ -55,7 +105,7 @@ export default function Login() {
                 Sign in
               </button>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </>
